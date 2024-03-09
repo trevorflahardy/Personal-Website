e9d04a88c8324d8fb007faf395298be8bd117b99
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-09 18:27:26 -0500

    Add more to ChaiComponent

--- src/components/ChaiComponent.vue
+++ src/components/ChaiComponent.vue
@@ -2,80 +2,163 @@
 import DiscordIcon from "@/assets/DiscordIcon.vue";
 
 const chaiUrl =
-    "https://discord.com/oauth2/authorize?client_id=728115804826239017&scope=bot&permissions=422964311";
+  "https://discord.com/oauth2/authorize?client_id=728115804826239017&scope=bot&permissions=422964311";
 </script>
 
 <template>
-    <div class="p-5 space-y-5">
-        <!--Description of Chai -->
-        <div class="glass-thick flex flex-col items-center justify-center p-5 rounded-xl snap-center">
-            <span class="text-white font-semibold text-xl"> Chai </span>
-            <span class="text-gray-300 text-md max-w-[70%] text-center">
-                A description of what Chai is. This is a very long description that
-                should take up multiple lines.
-            </span>
-            <!-- Holds some buttons for actions -->
-            <div class="flex flex-row items-center justify-center mt-3">
-                <a :href="chaiUrl" target="_blank" class="text-white font-semibold">
-                    <button class="button-glass">
-                        <DiscordIcon class="w-5 h-5 fill-white" />
-                        <span> Try on Discord </span>
-                    </button>
-                </a>
-            </div>
-        </div>
-
-        <!-- Holds cards about the box -->
-        <div class="flex flex-row items-stretch justify-center gap-5 snap-center">
-            <div class="basis-1/3 shadow-xl rounded-xl px-3 py-7 bg-gradient-to-br from-yellow-500 from-40 to-red-600 h-full">
-                <!-- TODO: Maybe refac this verbiage -->
-                <span class="text-white text-4xl font-semibold">
-                    Built in Python, expanded in Rust.
-                </span>
-            </div>
-        
-            <!-- Holds some cards that contain some jargon about what Chai is-->
-            <div class="basis-2/3 glass-thick shadow rounded-xl flex flex-row items-center justify-center overflow-hidden">
-                <div class="basis-2/5 h-full">
-                    <!--Image here or something, but for now placeholder-->
-                    <div class="h-full bg-gradient-to-br from-blue-500 to-indigo-800 shadow-xl"></div>
-                </div>
-
-                <div class="basis-3/5 p-4 space-y-3">
-                    <div>
-                        <div>
-                            <span class="text-white text-xl font-semibold">
-                            Some Sort of Title
-                        </span>
-                        </div>
-
-                        <div>
-                            <span class="text-gray-300 text-md">
-                            Some sort of descripiton here that can take up multiple lines.
-                        </span>
-                        </div>
-                    </div>
-
-                    <!--Maybe a button here that does something? -->
-                    <div class="button-glass shadow-xl w-fit">
-                        <span class="text-white font-semibold">An Action of Sorts</span>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <!-- Holds a preview of what Chai does (maybe macbook preview of it being used on Discord with some text next to it??)-->
-        <div class="snap-center p-5 flex flex-row justify-center items-center max-h-">
-            <div class="basis-2/5 text-center">
-                <span class="text-white text-4xl font-semibold">
-                    Engineered to be easy.
-                </span> 
-            </div>
-
-            <!-- An image or something that shows a preview (might photoshop?) -->
-            <div class="w-full h-96 rounded-xl bg-gradient-to-br from-gray-300 to-gray-600 shadow"/>
-        </div>
-
+  <div class="p-5 space-y-5">
+    <!--Description of Chai -->
+    <div
+      class="glass-thick flex flex-col items-center justify-center p-5 rounded-xl snap-center"
+    >
+      <span class="text-white font-semibold text-xl"> Chai </span>
+      <span class="text-gray-300 text-md max-w-[70%] text-center">
+        A description of what Chai is. This is a very long description that
+        should take up multiple lines.
+      </span>
+      <!-- Holds some buttons for actions -->
+      <!-- <div class="flex flex-row items-center justify-center mt-3">
+      </div> -->
     </div>
+
+    <!-- Holds cards about the box -->
+    <div class="flex flex-row items-stretch justify-center gap-5 snap-center">
+      <article
+        class="basis-1/3 shadow-xl rounded-xl px-5 py-12 bg-gradient-to-br from-yellow-500 from-40 to-red-600 h-full"
+      >
+        <!-- TODO: Maybe refac this verbiage -->
+        <span class="text-white text-4xl font-semibold text-balance">
+          Built in Python, expanded in Rust.
+        </span>
+      </article>
+
+      <!-- TODO: Holds some cards that contain some jargon about what Chai is-->
+      <article
+        class="basis-2/3 bg-gradient-to-br from-blue-500 to-indigo-800 shadow rounded-xl overflow-hidden p-5"
+      >
+        <div>
+          <div>
+            <span class="text-white text-2xl font-semibold">
+              Try on Discord
+            </span>
+          </div>
+
+          <div>
+            <span class="text-gray-300 text-md">
+              Some sort of descripiton about trying the bot on Discord.
+            </span>
+          </div>
+        </div>
+
+        <div>
+          <a :href="chaiUrl">
+            <button class="button-glass">
+              <DiscordIcon class="w-5 h-5 fill-white" />
+              <span class="text-white font-semibold"> Try on Discord </span>
+            </button>
+          </a>
+        </div>
+      </article>
+    </div>
+
+    <!-- Holds a preview of what Chai does (maybe macbook preview of it being used on Discord with some text next to it??)-->
+    <div class="snap-center p-5 gap-5 flex flex-row justify-center items-center">
+      <div class="basis-2/5 text-center">
+        <span class="text-white text-4xl font-semibold text-pretty">
+          Engineered to be easy.
+        </span>
+      </div>
+
+      <div
+        class="w-full h-96 rounded-xl shadow-2xl flex flex-row items-stretch"
+      >
+        <div class="px-2 py-5 glass-thick rounded-l-xl space-y-5 drop-shadow">
+          <!-- Circle Discord Logo-->
+          <div class="glass-thick h-5 w-5 rounded-full"></div>
+
+          <!-- Circle server logos -->
+          <div class="space-y-2">
+            <div class="glass-thick h-5 w-5 rounded-full"></div>
+            <div class="glass-thick h-5 w-5 rounded-full"></div>
+            <div class="glass-thick h-5 w-5 rounded-full"></div>
+            <div class="glass-thick h-5 w-5 rounded-full"></div>
+          </div>
+        </div>
+        <!-- Holds the server name and channels section-->
+        <div class="shadow-xl p-2 glass-semi-thick space-y-2">
+          <!-- Holds the channel name -->
+          <div class="px-2">
+            <span class="text-white font-semibold text-xs whitespace-nowrap">
+              Chai Preview
+            </span>
+          </div>
+
+            <div class="glass-thin-front rounded text-right p-2 shadow"></div>
+          <div class="glass-normal rounded text-right p-2 shadow"></div>
+          <div class="glass-normal rounded text-right p-2 shadow"></div>
+          <div class="glass-normal rounded text-right p-2 shadow"></div>
+          <div class="glass-normal rounded text-right p-2 shadow"></div>
+        </div>
+        <!-- Holds the actual messages and channel name at the top-->
+        <div class="w-full flex flex-col">
+          <!-- Mimics as if the channel name was there-->
+          <div class="p-3 glass-thick shadow rounded-tr-xl"></div>
+
+          <div class="h-full rounded-br-xl relative">
+            <!-- Message box -->
+            <div class="h-full w-full pb-12 pt-5 px-5 glass-normal flex">
+              <div class="self-end flex flex-row gap-2">
+                <!-- PFP -->
+                <div class="w-6 h-6 flex-none rounded-full bg-indigo-400"></div>
+
+                <!-- Actual embed -->
+                <div class="shadow rounded-xl flex flex-row w-fit h-fit">
+                  <div class="basis-[1%] bg-indigo-400 rounded-l-xl w-fill"></div>
+
+                  <div class="basis-[99%] glass-thick rounded-r-xl px-2 py-1">
+                    <!-- Inner workings of embed-->
+
+                    <div>
+                      <span
+                        class="text-white text-xs font-bold"
+                      >
+                        Chai's Fortnite Locker
+                    </span>
+                    </div>
+
+                    <div class="text-white text-xss">
+                        <span>
+                            <span class="font-semibold">Total Cosmetics</span>: 1,994, <span class="font-semibold">Filtered Cosmetics</span>: 1,777.
+                        </span>
+                    </div>
+
+                    <!-- The actual image of the embed -->
+                    <div class="flex justify-center pt-2">
+                        <img src="../assets/locker.png" class="w-fit h-auto"/>
+                    </div>
+
+                    <div class="pt-1 text-gray-300 text-xss">
+                        <span>
+                            <span class="font-semibold">51.77</span> seconds total, <span class="font-semibold">31.99</span> seconds for the image.
+                        </span>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Holds the fake "send message" button-->
+            <div class="flex absolute bottom-0 w-full px-5 py-3 space-x-5">
+              <!-- "Attachment" button - circle with a plus in it -->
+              <div
+                class="flex-none h-5 w-5 glass-thin-front rounded-full"
+              ></div>
+              <div class="h-5 w-full glass-thin-front rounded-full"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
