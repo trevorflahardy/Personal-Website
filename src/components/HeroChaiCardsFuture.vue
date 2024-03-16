13e075304b138c012e7a629727ff41ea2c2dc307
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-15 22:55:44 -0400

    Refactor HeroChaiCardsFuture.vue to use dynamic card contents

--- src/components/HeroChaiCardsFuture.vue
+++ src/components/HeroChaiCardsFuture.vue
@@ -1,30 +1,37 @@
+<script setup>
+const cardContents = [
+  {
+    title: "Why Chai?",
+    description:
+      "Some long text about why I made chai and why I think it's important and groundbreaking.",
+    id: 1,
+  },
+  {
+    title: "What's to Come?",
+    description:
+      "Some long text about what I plan to do with Chai in the future.",
+    id: 2,
+  },
+];
+</script>
+
 <template>
   <div class="flex flex-row gap-5 justify-center items-stretch snap-center">
-    <div
-      class="basis-1/2 glass-thick p-5 rounded-xl shadow-xl text-center space-y-2 translation hover:scale-[1.03] duration-300 ease-out"
-    >
-      <div>
-        <span class="text-white text-2xl font-semibold"> Why Chai? </span>
-      </div>
+    <div v-for="content in cardContents" :key="content.id">
+      <div
+        class="basis-1/2 glass-thick p-5 rounded-xl shadow-xl text-center space-y-2 translation hover:scale-[1.03] duration-300 ease-out"
+      >
+        <div>
+          <span class="text-white text-2xl font-semibold">
+            {{ content.title }}
+          </span>
+        </div>
 
-      <div class="">
-        <span class="text-gray-300">
-          Some long text about why I made chai and why I think it's important
-          and groundbreaking.
-        </span>
-      </div>
-    </div>
-    <div
-      class="basis-1/2 glass-thick p-5 rounded-xl shadow-xl text-center translation hover:scale-[1.03] duration-300 ease-out"
-    >
-      <div>
-        <span class="text-white text-2xl font-semibold"> What's to Come? </span>
-      </div>
-
-      <div class="">
-        <span class="text-gray-300">
-          Some long text about what I plan to do with Chai in the future.
-        </span>
+        <div>
+          <span class="text-gray-300">
+            {{ content.description }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
