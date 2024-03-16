da218cf78cf11a0b1b9fb1e0006e91d2a980defb
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-15 23:17:32 -0400

    Refactor HeroChai.vue component to use SlamTransition

--- src/components/HeroChai.vue
+++ src/components/HeroChai.vue
@@ -3,43 +3,14 @@ import HeroChaiDescription from "./HeroChaiDescription.vue";
 import HeroChaiCardsInfo from "./HeroChaiCardsInfo.vue";
 import HeroChaiPreview from "./HeroChaiPreview.vue";
 import HeroChaiCardsFuture from "./HeroChaiCardsFuture.vue";
+import SlamTransition from "./SlamTransition.vue";
 </script>
 
 <template>
-  <TransitionGroup name="slam" appear tag="div" class="p-5 space-y-5">
-    <HeroChaiDescription key="1" />
-    <HeroChaiCardsInfo key="2" />
-    <HeroChaiPreview key="3" />
-    <HeroChaiCardsFuture key="4" />
-  </TransitionGroup>
+  <SlamTransition tag="div" class="p-5 space-y-5">
+    <HeroChaiDescription />
+    <HeroChaiCardsInfo />
+    <HeroChaiPreview />
+    <HeroChaiCardsFuture />
+  </SlamTransition>
 </template>
-
-<style scoped>
-.slam-enter-active {
-  transition:
-    opacity 0.3s ease-in-out,
-    transform 0.6s ease-in-out;
-}
-.slam-enter-from {
-  opacity: 0;
-  transform: scale(1.05);
-}
-.slam-enter-to {
-  opacity: 1;
-  transform: scale(1);
-}
-
-/* Apply different delays based on the order of the elements */
-.slam-enter-active:nth-child(1) {
-  transition-delay: 0.1s;
-}
-.slam-enter-active:nth-child(2) {
-  transition-delay: 0.2s;
-}
-.slam-enter-active:nth-child(3) {
-  transition-delay: 0.3s;
-}
-.slam-enter-active:nth-child(4) {
-  transition-delay: 0.4s;
-}
-</style>
