d95178dc1b7897f3eb5ea7175c852d3ed2650f0f
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-16 16:29:53 -0400

    Update how glass is displayed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 export default {
-  darkMode: 'class',
-  content: ['./src/**/*.{js,jsx,ts,tsx,vue}', './index.html'],
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx,vue}", "./index.html"],
   theme: {
     extend: {
       backgroundImage: {
-        house: "url('/src/assets/HouseBG.jpg')"
+        "apt-day": "url('./assets/AptDay.png')",
+        "apt-night": "url('./assets/AptNight.png')",
       },
       fontSize: {
-        xss: ['10px', '12px']
-      }
+        xss: ["10px", "12px"],
+      },
+      colors: {
+        glass: {
+          regular: "#D9D9D9",
+          thin: "#c9c9c9",
+          semithick: "#5E5E5E",
+          thick: "#403f3f",
+        },
+      },
     },
     fontFamily: {
-      'sf-pro-display': ['SF Pro Display', 'Helvetica Neue', 'sans-serif'],
+      "sf-pro-display": ["SF Pro Display", "Helvetica Neue", "sans-serif"],
     },
   },
-  plugins: [
-    require('tailwindcss-3d'),
-  ]
-}
+  plugins: [require("tailwindcss-3d")],
+};
