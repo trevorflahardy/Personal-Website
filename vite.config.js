11b82b38334384a5c38f19ddda52e255f32558bc
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-15 21:17:28 -0400

    Remove unused dependencies and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import tailwindcss from 'tailwindcss';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Personal-Website/",
-  plugins: [
-    vue(),
-    vueJsx(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
