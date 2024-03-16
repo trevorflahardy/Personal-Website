d2c87624df0f49a42ed7b105520e543c1bf07862
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-16 16:30:55 -0400

    Move project item to own component

---
+++ src/components/HeroSidebarProject.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <button
+    class="flex flex-row items-center space-x-2 rounded-lg hover:glass-thin transition duration-300 ease-in-out hover:shadow-lg [transform: translateZ(30px)] py-2 px-3"
+  >
+    <!-- Grab the image slot, if it exists and insert it in a circle so that it fills the width and height of the button-->
+    <div class="w-7 h-7 rounded-full overflow-hidden">
+      <slot name="image" class="rounded-full object-cover overflow-hidden" />
+    </div>
+
+    <p class="text-white text-sm">{{ props.title }}</p>
+  </button>
+</template>
