f7534071f47fbd9bd5c7ba1be7c563a8b1b0e4dc
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-16 16:30:08 -0400

    Update for night and dark, change some layouts

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup>
-import Hero from './components/Hero.vue'
-import Footer from './components/Footer.vue'
+import Hero from "./components/Hero.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <main
-    class="bg-house bg-cover bg-clip-border h-screen w-full overflow-hidden"
+    class="bg-apt-day dark:bg-apt-night bg-cover bg-clip-border h-screen w-screen overflow-hidden"
   >
-    <div data-tilt data-tilt-max="0.4" data-tilt-perspective="1000" data-tilt-speed="500" data-tilt-reverse="true" class="h-screen w-screen absolute flex flex-col items-center justify-center content-center [transform-style: preserve-3d] [transform: perspective(1000px)]">
+    <div
+      data-tilt
+      data-tilt-max="0.4"
+      data-tilt-perspective="1000"
+      data-tilt-speed="500"
+      data-tilt-reverse="true"
+      class="h-screen w-screen absolute flex flex-col items-center justify-start content-center [transform-style: preserve-3d] [transform: perspective(1000px)] pt-10"
+    >
       <Hero />
       <Footer />
-
     </div>
   </main>
 </template>
