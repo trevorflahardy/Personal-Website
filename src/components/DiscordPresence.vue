3648d6584e1cb7332fec6d24322e76f3771bd3fa
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-16 00:21:57 -0400

    Refactor code to improve performance and readability

---
+++ src/components/DiscordPresence.vue
@@ -0,0 +1,15 @@
+<script setup>
+import axios from "axios";
+
+const discordId = 146348630926819328;
+const lanyardUrl = `https://api.lanyard.rest/v1/users/${discordId}`;
+
+// Async router GET request to the Lanyard API
+const response = await axios.get(lanyardUrl);
+
+// If the request fails, log the error
+const lanyardError = response.error;
+
+// Parse this into an object
+const lanyardData = response.data;
+</script>
