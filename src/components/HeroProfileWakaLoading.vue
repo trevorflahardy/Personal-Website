2453aed971e01db2238438054f0ccb550fc246f6
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-17 18:28:38 -0400

    Update loading component size

--- src/components/HeroProfileWakaLoading.vue
+++ src/components/HeroProfileWakaLoading.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="border-dashed border-2 border-gray-300 rounded-lg animate-pulse text-center p-10"
+    class="border-dashed border-2 border-gray-300 rounded-lg animate-pulse text-center p-36"
   >
     <span class="text-gray-300 font-semibold">Loading...</span>
   </div>
