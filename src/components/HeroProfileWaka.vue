0de5ca253a11561e9f3060d8ab23450f0102e49e
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-17 18:27:09 -0400

    Refactor WakaTime data fetching and calculation

--- src/components/HeroProfileWaka.vue
+++ src/components/HeroProfileWaka.vue
@@ -7,31 +7,32 @@ const wakaProfileUrl = "https://wakatime.com/@trevorflahardy";
 const wakaUrl =
   "https://wakatime.com/share/embeddable/trevorflahardy/969ad792-7d1c-4aaf-a45c-8b4b95858fdf.json";
 
-const response = await axios.get(wakaUrl);
-const allData = response.data;
-const data = allData["data"];
+async function fetchData(url) {
+  const response = await axios.get(url);
+  return response.data["data"];
+}
 
-const startDate = data["range"]["start"]; // Such as "2022-11-08T05:00:00Z"
+function calculateDaysSinceStart(startDate) {
+  const startDateTime = new Date(startDate);
+  const now = new Date();
+  return Math.floor((now - startDateTime) / (1000 * 60 * 60 * 24));
+}
 
-// Parse the date string into a Date object so we can get the days since the start date
-const startDateTime = new Date(startDate);
-const now = new Date();
-const daysSinceStart = Math.floor(
-  (now - startDateTime) / (1000 * 60 * 60 * 24)
-);
+const data = await fetchData(wakaUrl);
+const { range, grand_total, best_day } = data;
+
+const daysSinceStart = calculateDaysSinceStart(range.start);
 const humanReadableDaysSinceStart = daysSinceStart.toLocaleString();
 
 const humanReadableDailyAverage =
-  data["grand_total"]["human_readable_daily_average_including_other_language"];
+  grand_total.human_readable_daily_average_including_other_language;
 const humanReadableTotal =
-  data["grand_total"]["human_readable_total_including_other_language"];
+  grand_total.human_readable_total_including_other_language;
 
-const humanReadableBestDayTime = data["best_day"]["text"];
+const humanReadableBestDayTime = best_day.text;
 
-const bestDayDate = new Date(data["best_day"]["date"]);
-// Parse this date string into a Date object so we can get the day of the week and the month and year in the format, "Month, Day, Year"
-const bestDayDateTime = new Date(bestDayDate);
-const humanReadableBestDay = bestDayDateTime.toLocaleDateString("en-US", {
+const bestDayDate = new Date(best_day.date);
+const humanReadableBestDay = bestDayDate.toLocaleDateString("en-US", {
   weekday: "long",
   year: "numeric",
   month: "long",
@@ -78,7 +79,7 @@ const humanReadableBestDay = bestDayDateTime.toLocaleDateString("en-US", {
     <div class="flex items-center justify-end">
       <Button class="w-fit">
         <a :href="wakaProfileUrl" target="_blank" rel="noopener noreferrer">
-          View My WakaTime Profile
+          My WakaTime Profile
         </a>
       </Button>
     </div>
