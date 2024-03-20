1d51491670d51a444450c1df4894fa6a802d0a31
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-20 19:57:45 -0400

    Update HeroSidebarProject.vue with image styling

--- src/components/HeroSidebarProject.vue
+++ src/components/HeroSidebarProject.vue
@@ -12,8 +12,8 @@ const props = defineProps({
     class="flex flex-row items-center space-x-2 rounded-lg hover:glass-thin transition duration-300 ease-in-out hover:shadow-lg [transform: translateZ(30px)] py-2 px-3"
   >
     <!-- Grab the image slot, if it exists and insert it in a circle so that it fills the width and height of the button-->
-    <div class="w-7 h-7 rounded-full overflow-hidden">
-      <slot name="image" class="rounded-full object-cover overflow-hidden" />
+    <div class="w-7 h-7 rounded-full overflow-hidden shadow">
+      <slot name="image" class="object-cover" />
     </div>
 
     <p class="text-white text-sm">{{ props.title }}</p>
