ef4938eb34a279aeadd4edd9c3b9f2b31bf34889
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-31 19:51:02 -0400

    Refactor HeroImageMergerAbout component

--- src/components/HeroImageMergerAbout.vue
+++ src/components/HeroImageMergerAbout.vue
@@ -1,14 +1,5 @@
 <template>
-    <div class="rounded-xl w-full glass-thick shadow p-5 gap-y-2">
-        <div>
-            <span class="text-white font-semibold text-2xl">
-                About Image Merger
-            </span>
-        </div>
-        <div>
-            <span class="text-white">
-                This is a super duper long description!
-            </span>
-        </div>
+    <div class="rounded-xl w-full shadow flex flex-row items-stretch justify-center">
+
     </div>
 </template>
