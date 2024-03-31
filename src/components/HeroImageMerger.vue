ff293538dcde7268ca1df9c3bc2c84810d310723
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-31 14:18:48 -0400

    Add HeroImageMergerAbout component

--- src/components/HeroImageMerger.vue
+++ src/components/HeroImageMerger.vue
@@ -1,10 +1,12 @@
 <script setup>
 import SlamTransition from "./SlamTransition.vue";
 import HeroImageMergerIntro from "./HeroImageMergerIntro.vue";
+import HeroImageMergerAbout from "./HeroImageMergerAbout.vue";
 </script>
 
 <template>
   <SlamTransition tag="div" class="space-y-5">
     <HeroImageMergerIntro />
+    <HeroImageMergerAbout />
   </SlamTransition>
 </template>
---
+++ src/components/HeroImageMergerAbout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="rounded-xl w-full glass-thick shadow p-5 gap-y-2">
+        <div>
+            <span class="text-white font-semibold text-2xl">
+                About Image Merger
+            </span>
+        </div>
+        <div>
+            <span class="text-white">
+                This is a super duper long description!
+            </span>
+        </div>
+    </div>
+</template>
