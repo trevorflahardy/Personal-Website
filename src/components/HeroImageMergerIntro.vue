96129248e4bf91cd938551ff4e0fc3222d344f39
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-05 21:10:45 -0400

    Update spacing

--- src/components/HeroImageMergerIntro.vue
+++ src/components/HeroImageMergerIntro.vue
@@ -3,18 +3,11 @@ import HeroImageMergerImageAnimation from "./HeroImageMergerImageAnimation.vue";
 </script>
 
 <template>
-    <div class="flex flex-row items-center justify-center gap-7 pt-5">
-        <div class="text-left text-6xl font-bold text-white">
-            <div>   
-                <span>
-                    Image
-                </span>
-            </div>
-            <div>
-                <span>
-                    Merger
-                </span>
-            </div>
+    <div class="flex flex-row items-center justify-center pt-5 gap-10">
+        <div class="text-center text-6xl font-bold text-white">
+            <span>
+                Image <br> Merger
+            </span>
         </div>
 
         <HeroImageMergerImageAnimation />
