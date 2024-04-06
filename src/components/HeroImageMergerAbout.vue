64f57994853a2ca7b1648b0955e1e9a920c80317
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-04-05 21:13:16 -0400

    Update image merger information in HeroImageMergerAbout.vue

--- src/components/HeroImageMergerAbout.vue
+++ src/components/HeroImageMergerAbout.vue
@@ -10,21 +10,25 @@ import Button from './Button.vue';
         </div>
 
         <!-- Hold the description of the image merger -->
-        <div class="w-full glass-thick rounded-r-xl shadow p-5 space-y-2">
-            <div>
-                <span class="text-white text-2xl font-semibold">
-                    What is an Image Merger?
-                </span>
+        <div class="w-full glass-thick rounded-r-xl shadow p-5 space-y-5">
+            <div class="space-y-2">
+                <div>
+                    <span class="text-white text-2xl font-semibold">
+                        What is an Image Merger?
+                    </span>
+                </div>
+
+                <div>
+                    <span class="text-white">
+                        Image Merger is a <span class="text-green-400 font-medium">blazingly fast</span> and easy-to-use Rust crate that merges many images into one
+                        larger
+                        image. Boosted by paralleling, it can merge thousands of images in milliseconds. Its development
+                        is
+                        centered around memory efficiency and speed, as well as ease of use.
+                    </span>
+                </div>
             </div>
 
-            <div>
-                <span class="text-white">
-                    Image Merger is a blazingly fast and easy-to-use Rust crate that merges many images into one larger
-                    image. Boosted by paralleling, it can merge thousands of images in milliseconds. Its development is
-                    centered around memory efficiency and speed, as well as ease of use.
-                </span>
-            </div>
-        
             <!-- TODO: Logos for the buttons -->
             <div class="flex flex-row items-center justify-start gap-5">
                 <Button link="https://docs.rs/image-merger/latest/image_merger/">
