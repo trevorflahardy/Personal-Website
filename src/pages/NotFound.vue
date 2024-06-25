b57270b908a09914e26c2897ce6dd1d3b06d83b1
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-06-25 15:19:52 -0400

    Add a NotFound screen for special users

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Button from '@/components/Button.vue';
+</script>
+
+<template>
+    <div class="p-5">
+        <h1 class="title">Hold on there, chief!</h1>
+        <p class="subtitle">
+            You seem to have found a page that doesn't exist! Don't worry, it happens to the best of us. Click on the
+            button below to go back to safety. If you think this is a mistake, it works on my machine so it's probably
+            your fault.
+        </p>
+
+        <router-link :to="{ name: 'profile' }">
+            <Button content="Take me back to safety" icon="pi-home" />
+        </router-link>
+    </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
     // The main page of the app. Shows my profile.
-    { path: "/", component: () => import('@/pages/profile/HeroProfile.vue'), name: 'profile' },
+    { path: "/", name: 'profile', component: () => import('@/pages/profile/HeroProfile.vue') },
     // NOTE: Maybe make these dynamic in the future?
-    { path: "/p/chai", component: () => import('@/pages/chai/Chai.vue'), name: 'project-chai' },
+    { path: "/p/chai", name: 'project-chai', component: () => import('@/pages/chai/Chai.vue') },
+
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: () => import('@/pages/NotFound.vue') },
 ]
 
 export const router = createRouter({
