fd2629cfeb561adab0bca97d72f0a3193ff56b62
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-07-18 01:10:46 -0400

    refactor: Remove unused transition code in Hero.vue -> will be refactoring

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Transition } from "vue";
 import Sidebar from "./sidebar/Sidebar.vue";
 </script>
 
@@ -11,43 +10,9 @@ import Sidebar from "./sidebar/Sidebar.vue";
       <div
         class="h-full overflow-y-scroll no-scrollbar scroll-smooth snap-y sm:p-5 sm:px-2 py-12 sm:py-12 md:py-16 xl:py-5 px-3">
         <router-view v-slot="{ Component }">
-          <Transition name="fade">
-            <component :is="Component" />
-          </Transition>
+          <component :is="Component" />
         </router-view>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.fade-leave-active {
-  transition: opacity 0.3s;
-}
-
-.fade-leave-from {
-  opacity: 1;
-}
-
-.fade-leave-to {
-  opacity: 0;
-}
-
-.fade-enter-active {
-  transition:
-    opacity 0.3s,
-    /* transform 0.3s; */
-  ;
-  transition-delay: 0.3s;
-}
-
-.fade-enter-from {
-  opacity: 0;
-  /*transform: scale(0.95); */
-}
-
-.fade-enter-to {
-  /*transform: scale(1); */
-  opacity: 1;
-}
-</style>
