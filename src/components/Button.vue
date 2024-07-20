20694355368893ada84b2437ed9ea727ab3a97c6
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-07-20 17:57:18 -0400

    refactor: Update Button component to support background color customization

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,31 +1,27 @@
 <script setup lang="ts">
 interface Props {
-  link?: string;
-  content?: string;
-  icon?: string;
+  link?: string | null,
+  content?: string | null,
+  icon?: string | null,
+  background?: string | null,
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const buttonBackground = props.background || "bg-gray-300/10";
+const buttonClass = `py-3 px-5 rounded-xl shadow transition-all duration-400 hover:scale-105 ease-in-out ${buttonBackground}`;
 </script>
 
 <template>
   <!-- If the props.link is not null, we can wrap the button in it -->
-  <div v-if="link">
-    <a :href="link">
-      <div class="button-glass text-white">
-        <i v-if="icon" :class="`pi ${icon}`"></i>
-
-        <p v-if="content" class="subtitle m-0 font-semibold">
-          {{ content }}
-        </p>
-      </div>
+  <button :class="buttonClass">
+    <a v-if="link" :href="link" class="text-white subtitle m-0 flex flex-row items-center justify-center gap-2">
+      <i v-if="icon" :class="`pi ${icon}`"></i>
+      <p v-if="content">{{ content }}</p>
     </a>
-  </div>
-
-  <!-- If the props.link is null, we can just render the button -->
-  <div v-else class="button-glass text-white subtitle m-0">
-    <p v-if="content" class="font-semibold">
-      {{ content }}
-    </p>
-  </div>
+    <div v-else class="text-white subtitle m-0 flex flex-row items-center justify-center gap-2">
+      <i v-if="icon" :class="`pi ${icon}`"></i>
+      <p v-if="content">{{ content }}</p>
+    </div>
+  </button>
 </template>
