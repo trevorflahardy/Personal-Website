049d7a236028b91a268ddac63e2a27e44e2de1f9
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-07-20 17:58:37 -0400

    refactor: Refactor Chai.vue to improve layout and add action buttons

--- src/pages/chai/Chai.vue
+++ src/pages/chai/Chai.vue
@@ -4,21 +4,21 @@ import Preview from './Preview.vue';
 import DeepDive from './DeepDive.vue';
 import Button from "@/components/Button.vue";
 
-const chaiInviteLink = "https://discord.com/oauth2/authorize?client_id=728115804826239017&scope=bot&permissions=422964311"
+const chaiInviteLink = "https://discord.com/oauth2/authorize?client_id=728115804826239017&scope=bot&permissions=422964311";
 </script>
 
 <template>
   <PageLayoutSpacer>
     <!-- Holds the intro to Chai. 
     TODO; Refactor this so that it's:
-    1. in its own component, and
+    1. in its own component, ands
     2. so that it has "Chai" and stuff on the side of some sort of logo or SVG-icon. Create a modern
     feel with it, and have the preview below it for separation.-->
     <div class="w-full flex flex-row items-center justify-around">
       <!-- Holds the header and "call to action" button(s)-->
       <div class="flex flex-col items-center justify-center max-w-3xl pt-3">
         <h1 class="title text-center mb-3 text-pretty">
-          Meet Chai, the Fortnite authentication Discord Bot.
+          Meet <a class="text-chai">Chai</a>, the Fortnite authentication Discord Bot.
         </h1>
 
         <p class="subtitle text-center mb-3">
@@ -26,7 +26,12 @@ const chaiInviteLink = "https://discord.com/oauth2/authorize?client_id=728115804
           all within Discord.
         </p>
 
-        <Button :link="chaiInviteLink" content="Invite Chai" icon="pi-discord" class="w-40 md:w-60" />
+        <!-- Some action buttons -->
+        <div class="flex flex-row gap-5">
+          <Button :link="chaiInviteLink" content="Invite Chai" icon="pi-discord" />
+          <Button link="https://github.com/users/trevorflahardy/projects/11" content="Development Board"
+            icon="pi-book" />
+        </div>
       </div>
     </div>
 
