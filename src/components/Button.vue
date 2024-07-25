01041f531d19c859e80a269fda24da0ad6d45b3d
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-07-25 16:56:48 -0400

    Add functionality for text color and button opening in new tab

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -4,22 +4,28 @@ interface Props {
   content?: string | null,
   icon?: string | null,
   background?: string | null,
+  textColor?: string | null,
+  openInNewTab?: boolean | null
 }
 
 const props = defineProps<Props>();
 
-const buttonBackground = props.background || "bg-gray-300/10";
-const buttonClass = `py-3 px-5 rounded-xl shadow transition-all duration-400 hover:scale-105 ease-in-out ${buttonBackground}`;
+const buttonBackground = props.background || "bg-gray-800/40 dark:bg-gray-200/5";
+const buttonClass = `py-3 px-5 rounded-xl shadow transition-all duration-500 hover:scale-105 ease-in-out ${buttonBackground}`;
+
+// Want the default behavior to open in a new tab
+const _shouldOpenInNewTab = props.openInNewTab || true;
 </script>
 
 <template>
   <!-- If the props.link is not null, we can wrap the button in it -->
   <button :class="buttonClass">
-    <a v-if="link" :href="link" class="text-white subtitle m-0 flex flex-row items-center justify-center gap-2">
+    <a v-if="link" :href="link" :target="_shouldOpenInNewTab ? '_blank' : ''"
+      class="subtitle m-0 flex flex-row items-center justify-center gap-2" :class="textColor">
       <i v-if="icon" :class="`pi ${icon}`"></i>
       <p v-if="content">{{ content }}</p>
     </a>
-    <div v-else class="text-white subtitle m-0 flex flex-row items-center justify-center gap-2">
+    <div v-else class="subtitle m-0 flex flex-row items-center justify-center gap-2" :class="textColor">
       <i v-if="icon" :class="`pi ${icon}`"></i>
       <p v-if="content">{{ content }}</p>
     </div>
