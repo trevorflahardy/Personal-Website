4214e795076f85e748cf70fd3f09c277d5a78bc3
NextChai <futureengineerdlc@gmail.com>
2024-09-20 20:29:57 -0400

    Refactor sidebar content width in App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import Background from './components/Background.vue';
     <Background class="z-10 absolute" />
 
     <div
-      class="z-20 h-screen w-screen relative flex flex-col items-center justify-start content-center [transform-style: preserve-3d] [transform: perspective(1000px)] pt-10 px-5 xl:pt-[5%] xl:pb-[13%] xl:px-[5%] overflow-hidden">
+      class="z-20 h-screen w-screen relative flex flex-col items-center justify-start content-center pt-10 px-5 xl:pt-[5%] xl:pb-[13%] xl:px-[5%] overflow-hidden">
       <!-- We're using Vue Router here. Thus, to ensure that we always have a proper footer and sidebar, we're going to embed the router view in the Hero. -->
       <Hero />
       <Footer />
