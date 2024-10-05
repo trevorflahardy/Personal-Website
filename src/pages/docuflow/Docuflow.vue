244bc213e16ea6784fadec6375c337156361c51c
NextChai <futureengineerdlc@gmail.com>
2024-10-04 20:48:29 -0400

    Update Docuflow.vue with additional margin bottom for larger screens

--- src/pages/docuflow/Docuflow.vue
+++ src/pages/docuflow/Docuflow.vue
@@ -59,7 +59,7 @@ const docuflowInfoCards = [
 
         <!-- Below it we see some previews of docuflow. This has two images: (1) the documentation
          part of it in the back, and the home page stacked on the front of it. -->
-        <div class="w-full h-fit space-y-5 md:px-5 lg:px-10 relative xl:!mb-16">
+        <div class="w-full h-fit space-y-5 md:px-5 lg:px-10 relative xl:!mb-16 2xl:!mb-24">
             <img src="/docuflow-home.png" alt="Docuflow home page preview."
                 class="xl:w-[85%] z-10 rounded-xl xl:rounded-[40px]" />
             <img src="/docuflow-docs.png" alt="Docuflow documentation page preview."
