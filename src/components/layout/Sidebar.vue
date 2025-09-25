b339d95b52c6337211f207125615af772880e11f
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2025-09-25 14:16:40 -0400

    feat: add sidebar component with Glass integration

---
+++ src/components/layout/Sidebar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Glass from "../ui/Glass.vue";
+</script>
+
+<template>
+  <!-- Holds our sidebar, which contains navigation links and other relevant information.
+
+   The whole goal here is to mimic apple's settings theme here-->
+
+  <Glass class="w-full h-full rounded-xl text-white p-5"> I am sidebar </Glass>
+</template>
