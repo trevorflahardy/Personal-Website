7bf31f6e43db2c04649a2f14234b01c0164149fd
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2025-09-25 14:17:03 -0400

    refactor: remove PostCSS configuration and update Tailwind CSS integration
    - Deleted the PostCSS configuration file as it is no longer needed.
    - Updated the main CSS file to import Tailwind CSS directly instead of using the @tailwind directives.
    - Changed the Vite configuration to import Tailwind CSS from the new package location.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import svgLoader from 'vite-svg-loader'
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
